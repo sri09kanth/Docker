64dd8fe80713046a260cc3acdf3eda6c579f6293
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-01-17 09:53:07 +0530

    Update 5_args_websetup.sh

--- 5_args_websetup.sh
+++ 5_args_websetup.sh
@@ -3,9 +3,7 @@
 # Variable Declaration
 PACKAGE="httpd wget unzip"
 SVC="httpd"
-#URL='https://www.tooplate.com/zip-templates/2098_health.zip'
-#ART_NAME='2098_health'
-TEMPDIR="/tmp/webfiles"
+
 
 
 echo "########################################"
